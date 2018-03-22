719382bc00024be8c531e7b8d7ad72ba0e0e4661
Christian Deocampo <christian.deocampo@gmail.com>
2018-03-22 10:42:50 +0800

    Adds puppet modules and manifests

---
+++ puppet-modules/memory_monitor/manifests/init.pp
@@ -0,0 +1,22 @@
+class memory_monitor {
+    package { 'vim-enhanced':
+        ensure => 'installed',
+        name => 'vim-enhanced',
+    }
+    package { 'curl':
+        ensure => 'installed',
+        name => 'curl',
+    }
+    package { 'git':
+        ensure => 'installed',
+        name => 'git',
+    }
+    user { 'monitor':
+        ensure => 'present',
+        shell => '/bin/bash',
+        home => '/home/monitor'
+    }
+    file { '/home/monitor/scripts'
+        ensure => 'directory'
+    }
+}
