ae070817468218ace00348fd8a3b68b0c6f14dfe
Christian Deocampo <christian.deocampo@gmail.com>
2018-03-22 11:14:52 +0800

    Appends newline in /ets/hosts

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yes | yum -y install puppet
 hostname bpx.server.local
 echo "bpx.server.local" >> /etc/resolv.conf
-echo "127.0.0.1     bpx.server.local"
+echo "127.0.0.1     bpx.server.local" >> /etc/hosts
 sed -i -e 's/localhost.localdomain/bpx.server.local/' /etc/sysconfig/network
 service network restart
