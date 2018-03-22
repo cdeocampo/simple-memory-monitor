d09d39e6d49871809a8300cebef394b84c158dc3
Christian Deocampo <christian.deocampo@gmail.com>
2018-03-22 19:07:59 +0800

    Adds soft link to memory_check script

--- puppet-modules/memory_monitor/manifests/init.pp
+++ puppet-modules/memory_monitor/manifests/init.pp
@@ -27,4 +27,11 @@ class memory_monitor {
         mode => 0755,
         require => Exec["retrieve_memory_monitor"],
     }
+    file { '/home/monitor/src':
+        ensure => 'directory',
+    }
+    file { '/home/monitor/src/my_memory_check':
+        ensure => 'link',
+        target => '/home/monitor/scripts/memory_check'
+    }
 }
